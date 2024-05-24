69eb6fabc32559225a7fef5997a030b229eebc9e
willie <willief@gmail.com>
2024-05-24 20:56:14 +0100

    initial commit for kill_and_restart.sh

---
+++ kill_and_restart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DISCORD_ID="readable_discord_user_name"
+MINPORT=33300
+MAXPORT=33309
+for i in $(ls ~/.local/share/safe/node/)
+do
+  iPORT=$(cat ~/.local/share/safe/node/$i/PORT)
+  if [[ $iPORT -le $MAXPORT && $iPORT -ge $MINPORT ]]; then
+    echo ; echo "Replacing node with peer-id $i at port $iPORT..."
+    iPID=$(lsof ~/.local/share/safe/node/$i/logs/safenode.log | grep "safenode " | grep -o "[0-9]*" | head -1)
+    kill $iPID
+    sleep 1
+    echo "Starting replacement node with peer-id $i at port $iPORT..."
+    screen -LdmS "safenode$iPORT" safenode --owner $DISCORD_ID  --port $iPORT --root-dir ~/.local/share/safe/node/$i --log-output-dest ~/.local/share/safe/node/$i/logs --max_log_files=9 --max_archived_log_files=0
+    iPID=$(lsof ~/.local/share/safe/node/$i/logs/safenode.log | grep "safenode " | grep -o "[0-9]*" | head -1)
+    echo "$iPID" > ~/.local/share/safe/node/$i/PID
+    sleep 1
+  fi
+done
--- toolbox.sh
+++ toolbox.sh
@@ -2,10 +2,24 @@
 
 # Define the variables and their default values
 MINPORT=12000
-MAXPORT=12020
+MAXPORT=1202
 NODE_DIR="$HOME/.local/share/safe/node/"
 
+#Whiptail setup
+export NEWT_COLORS='
+window=,white
+border=black,white
+textbox=black,white
+button=black,white
+'
+export TERM=xterm-256color
+
 # Create a whiptail script to ask for user input
+whiptail --title "Node Toolbox" \
+         --inputbox "User or system mode? If you are in any doubt, accept the default user mode:" 10 60 3 $(printf '%s\n' "${options[@]}")
+
+
+
 whiptail --title "Node Toolbox" \
          --inputbox "Enter the minimum port number (integer only): " 10 60 "$MINPORT" 3>&1 1>&2 2>&3 min_port
 if [[ $min_port =~ ^[1-9][0-9]*$ && $min_port -ge 12000 && $min_port < 49151 ]]; then
@@ -22,13 +36,13 @@ else
     echo "Invalid input, please enter a positive integer greater than $MINPORT" >&2
 fi
 
-for i in $(ls ~/.local/share/safe/node/)
+for i in $(ls $NODE_DIR)
 do
-  iPID=$(lsof ~/.local/share/safe/node/$i/logs/safenode.log | grep "safenode " | grep -o "[0-9]*" | head -1)
+  iPID=$(lsof $NODE_DIR$i/logs/safenode.log | grep "safenode " | grep -o "[0-9]*" | head -1)
   iPORT=$(netstat -lnup 2> /dev/null | grep "$iPID/safenode" | grep -o "[0-9]*" | head -7 | tail -n1)
   if [[ $iPORT -le $MAXPORT && $iPORT -ge $MINPORT ]]; then
     echo "stopping node peer-id $i at port $iPORT"
-    echo "$iPORT" > ~/.local/share/safe/node/$i/PORT
+    echo "$iPORT" > $NODE_DIR$i/PORT
     kill $iPID
   else
     echo "skipping pid $iPID on port $iPORT (not in range or not running)"
@@ -38,12 +52,12 @@ done
 exit 0     #bail here for now
 
 PORTNUMBER=30910
-for i in $(ls ~/.local/share/safe/node/)
+for i in $(ls $NODE_DIR)
 do
-  iPORTLOG=$(cat ~/.local/share/safe/node/$i/PORT) || iPORTRUN=$(netstat -lnup 2> /dev/null | grep "$iPID/safenode" | grep -o "[0-9]*" | head -7 | tail -n1)
+  iPORTLOG=$(cat $NODE_DIR$i/PORT) || iPORTRUN=$(netstat -lnup 2> /dev/null | grep "$iPID/safenode" | grep -o "[0-9]*" | head -7 | tail -n1)
   echo -n "."
   if [[ $iPORTRUN -eq $PORTNUMBER || $iPORTLOG -eq $PORTNUMBER ]]; then
-    RESULT="$HOME/.local/share/safe/node/$i/logs/safenode.log"
+    RESULT="$HOM$NODE_DIR$i/logs/safenode.log"
     echo ; echo "$RESULT"
     kill -INT $$  #break
   fi
